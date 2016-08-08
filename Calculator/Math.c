3b1f36c9ee6a243f4cbd5ba9703d54e38c23a45f
rsampdev <rsampdev@gmail.com>
2016-08-08 13:48:20 -0400

    Added Math.c and basic math methods

---
+++ Calculator/Math.c
@@ -0,0 +1,28 @@
+//
+//  Math.c
+//  Calculator
+//
+//  Created by Rodney Sampson on 8/8/16.
+//  Copyright © 2016 Rodney Sampson II. All rights reserved.
+//
+
+#include "Math.h"
+
+double addition(int a, int b) {
+    return a + b;
+}
+
+
+double subtraction(int a, int b) {
+    return a - b;
+}
+
+
+double multiplication(int a, int b) {
+    return a * b;
+}
+
+
+double division(int a, int b) {
+    return a / b;
+}
---
+++ Calculator/Math.h
@@ -0,0 +1,14 @@
+//
+//  Math.h
+//  Calculator
+//
+//  Created by Rodney Sampson on 8/8/16.
+//  Copyright © 2016 Rodney Sampson II. All rights reserved.
+//
+
+#ifndef Math_h
+#define Math_h
+
+#include <stdio.h>
+
+#endif /* Math_h */
